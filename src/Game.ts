a21ac8426c213b2fa2aa824763cabd475859c286
Chris Harding <chris.harding@cazoo.co.uk>
2021-03-23 15:57:28 +0000

    refactor comments and long method

--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,10 @@
 export class Game {
+
+    /* SMELL: TEMPORARY FIELD */
     private _lastSymbol: string = ' ';
     private _board: Board = new Board();
 
+    /* SMELL: SWITCH, LONG PARAMETER LIST, COMMENTS  */
     public Play(symbol: string, x: number, y: number) : void {
         //if first move
         if (this._lastSymbol == ' ') {
@@ -24,43 +27,35 @@ export class Game {
         this._board.AddTileAt(symbol, x, y);
     }
 
+    /* SMELL: DUPLICATED CODE, LONG METHOD, FEATURE ENVY, MESSAGE CHAIN, COMMENTS */
     public Winner() : string {
-        //if the positions in first row are taken
-        if (this._board.TileAt(0, 0)!.Symbol != ' ' &&
-                this._board.TileAt(0, 1)!.Symbol != ' ' &&
-                this._board.TileAt(0, 2)!.Symbol != ' ') {
-            //if first row is full with same symbol
-            if (this._board.TileAt(0, 0)!.Symbol ==
-                    this._board.TileAt(0, 1)!.Symbol &&
-                    this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol) {
-                return this._board.TileAt(0, 0)!.Symbol;
-            }
+        
+        let winner = ' ';
+
+        winner = this.WinnerOnRow(0);
+        if (winner != ' ') {
+            return winner;
         }
 
-        //if the positions in first row are taken
-        if (this._board.TileAt(1, 0)!.Symbol != ' ' &&
-                this._board.TileAt(1, 1)!.Symbol != ' ' &&
-                this._board.TileAt(1, 2)!.Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (this._board.TileAt(1, 0)!.Symbol ==
-                    this._board.TileAt(1, 1)!.Symbol &&
-                    this._board.TileAt(1, 2)!.Symbol ==
-                            this._board.TileAt(1, 1)!.Symbol) {
-                return this._board.TileAt(1, 0)!.Symbol;
-            }
+        winner = this.WinnerOnRow(1);
+        if (winner != ' ') {
+            return winner;
         }
 
-        //if the positions in first row are taken
-        if (this._board.TileAt(2, 0)!.Symbol != ' ' &&
-                this._board.TileAt(2, 1)!.Symbol != ' ' &&
-                this._board.TileAt(2, 2)!.Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (this._board.TileAt(2, 0)!.Symbol ==
-                    this._board.TileAt(2, 1)!.Symbol &&
-                    this._board.TileAt(2, 2)!.Symbol ==
-                            this._board.TileAt(2, 1)!.Symbol) {
-                return this._board.TileAt(2, 0)!.Symbol;
-            }
+        winner = this.WinnerOnRow(2);
+        if (winner != ' ') {
+            return winner;
+        }
+
+        return winner;
+    }
+
+    /* SMELL: FEATURE ENVY, MESSAGE CHAIN */
+    WinnerOnRow(row: number): string {
+        if (this._board.TileAt(row, 0)!.Symbol != ' ' &&
+            this._board.TileAt(row, 0)!.Symbol == this._board.TileAt(row, 1)!.Symbol &&
+            this._board.TileAt(row, 2)!.Symbol == this._board.TileAt(row, 1)!.Symbol) {
+            return this._board.TileAt(row, 0)!.Symbol;
         }
 
         return ' ';
@@ -69,6 +64,7 @@ export class Game {
 
 interface Tile
 {
+    /* SMELL: PRIMITIVE OBSESSION */
     X: number;
     Y: number;
     Symbol: string;
@@ -94,6 +90,7 @@ class Board
         return this._plays.find((t:Tile) => t.X == x && t.Y == y)!
     }
 
+    /* SMELL: LONG PARAMETER LIST */
     public AddTileAt(symbol: string, x: number, y: number) : void
     {
         const tile : Tile = {X :x, Y:y, Symbol:symbol};
